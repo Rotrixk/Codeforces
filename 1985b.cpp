c98c85e2ae73973b160dd262794f5381752860b3
Sakibul Hassan Rifat <92259707+iamRiifat@users.noreply.github.com>
2024-07-04 01:37:52 +0600

    not accepted

---
+++ 1985b.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int temp=2,sum=0;
+        for(int i=temp;i<=n;i++){
+            
+        }
+    }
+}
